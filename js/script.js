d98ecf378c6260c41468542cc485ca6c1e3c955a
MaxicN <91767560+MaxicN@users.noreply.github.com>
2021-11-11 22:50:18 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@ var recognition = new SpeechRecognition();
 var traducir = document.getElementById('traducir');
 var escuchar = document.getElementById('escuchar');
 var resultado = document.getElementById('resultado');
-const codigoIdiomas = ["en", "pt", "ru", "zh", "ja", "bn", "fil", "ko", "it", "fr"];
-const idiomas = ["US English Female", "Portuguese Female", "Russian Female", "Chinese Female", "Japanese Female", "Hindi Female", "Filipino Female", "Korean Female", "Italian Female", "French Female"];
+const codigoIdiomas = ["es", "en", "pt", "ru", "zh", "ja", "bn", "fil", "ko", "it", "fr"];
+const idiomas = ["Spanish Latin American Female","US English Female", "Portuguese Female", "Russian Female", "Chinese Female", "Japanese Female", "Hindi Female", "Filipino Female", "Korean Female", "Italian Female", "French Female"];
 responsiveVoice.setDefaultRate(0.8);
 
 /*Si se presiona boton de microfono */
 speak.addEventListener('click', function () {
     recognition.start();
+    var idiomaEntrada = document.querySelector('#idiomaEntrada');
+    for (var idiomaNum=0 in codigoIdiomas){ /* recorrera los codigos de los idiomas, asignandole un numero a cada uno de 0 a n */
+        if (codigoIdiomas[idiomaNum] == idiomaEntrada.value){
+            recognition.lang = codigoIdiomas[idiomaNum];
+            console.log(codigoIdiomas[idiomaNum]);
+            break;
+        }
+    }
     speak.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16"><path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/><path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/></svg>';
     textarea.innerHTML = 'Escuchando...';
     /* si se escribio en el textarea bajo el microfono*/
@@ -31,14 +39,14 @@ recognition.onresult = function (e) {
 /*Si se presiona boton de traducir */
 traducir.addEventListener('click', function () {
     var idioma = document.querySelector('#idioma');
+    /* var idiomaEntrada = document.querySelector('#idiomaEntrada'); */
     escuchar.removeAttribute('disabled');
     escuchar.setAttribute("style", "animation-name: desbloquear;animation-duration: 1.5s; animation-fill-mode: forwards;cursor:pointer;");
-    var langCodes = idioma.value;
-    console.log("langCodes:"+langCodes);
     var text = textarea.value;
-    var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=' + langCodes + '&dt=t&q=' + text;
+    var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl='+idiomaEntrada.value+'&tl=' + idioma.value + '&dt=t&q=' + text;
     $.get(url, function (data) {
         resultado.innerHTML = data[0][0][0];
+        console.log("datos: "+data);
     })
 
 });
